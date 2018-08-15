944ec333b64fb68425fa616db2ef8991241241ab
Teddy Otieno <teddy.asola@andela.com>
2018-08-15 21:06:37 +0300

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
-	mode: "development",
-	entry: "./src/js/app.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js"
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({
-			template: "./src/index.pug"
-		}),
-		new MiniCssExtractPlugin()
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: "babel-loader"
-			},
-			{
-				test: /\.pug$/,
-				use: "pug-loader"
-			},
-			{
-				test: /\.(sa|sc|c)ss$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-			}
-		]
-	}
+  mode: "development",
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.pug"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.pug$/,
+        use: "pug-loader"
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
 };
 
 module.exports = config;
