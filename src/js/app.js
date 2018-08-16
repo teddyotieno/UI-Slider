e9f80662d993cb8db23c7a192b0652245f68839c
Teddy Otieno <teddy.asola@andela.com>
2018-08-16 14:34:24 +0300

    swap out the cards depending on the right or left button clicked

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,34 @@
 import styles from "../sass/app.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let cards = document.querySelectorAll("div.card");
+  let parentContainerWidth = document.getElementById("container").offsetWidth;
+
+  // create initial state for the elements
+  cards.forEach((item, index) => {
+    if (index === 0) {
+      item.style.left = "0px";
+    } else if (index === 1) {
+      item.style.left = `${parentContainerWidth}px`;
+    } else {
+      item.style.left = `${parentContainerWidth * 2}px`;
+    }
+  });
+
+  const rightAngleButton = document.getElementById("right-angle");
+  const leftAngleButton = document.getElementById("left-angle");
+
+  rightAngleButton.addEventListener("click", () => {
+    cards.forEach(item => {
+      item.style.left = `${parseInt(item.style.left, 10) +
+        parentContainerWidth}px`;
+    });
+  });
+
+  leftAngleButton.addEventListener("click", () => {
+    cards.forEach(item => {
+      item.style.left = `${parseInt(item.style.left, 10) -
+        parentContainerWidth}px`;
+    });
+  });
+});
