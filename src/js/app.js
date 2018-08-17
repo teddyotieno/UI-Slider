52039e5e00bdf67e9e72aa5fe60d9474f5478c15
Teddy Otieno <teddy.asola@andela.com>
2018-08-17 17:52:29 +0300

    refactor code

--- src/js/app.js
+++ src/js/app.js
@@ -4,46 +4,64 @@ window.onload = function() {
   // prevent FOUC phenomena
   document.querySelector(".no-fouc").classList.remove("no-fouc");
 
+  // global variables
   const cards = document.querySelectorAll("div.card-wrapper");
   const cardWidth = document.getElementById("container").offsetWidth;
   let counter = 1;
 
-  // create initial state for the elements
-  cards.forEach((item, index) => {
-    item.style.transform = `translateX(${cardWidth * index}px)`;
-  });
+  function setInitialStateForCards() {
+    // tranlslate/spread cards on the x plane by multiplying the card width by the index of the card
+    cards.forEach((item, index) => {
+      item.style.transform = `translateX(${cardWidth * index}px)`;
+    });
+  }
 
   function toggleCardsFunctionality() {
     const rightAngleButton = document.getElementById("right-angle");
     const leftAngleButton = document.getElementById("left-angle");
 
+    function shuffleCards(movementDirection) {
+      if (movementDirection === "rightToLeft") {
+        cards.forEach(function(card, index) {
+          translateCard(card, index, function(a, b) {
+            return a - b;
+          });
+        });
+      } else {
+        cards.forEach(function(card, index) {
+          translateCard(card, index, function(a, b) {
+            return a + b;
+          });
+        });
+      }
+    }
+
+    function translateCard(card, index, cb) {
+      let newTranslateXValue = cb(
+        parseInt(card.style.transform.replace(/[^\d.-]/g, ""), 10),
+        cardWidth
+      );
+      card.style.transition = "transform 1000ms cubic-bezier(0.82, 0, 0, 0.96)";
+      card.style.transform = `translateX(${newTranslateXValue}px)`;
+      // if (index === counter) {
+      //   card.classList.add("activate");
+      // }
+    }
+
     rightAngleButton.addEventListener("click", () => {
       if (counter > 1) {
-        cards.forEach(item => {
-          let newTranslateXValue =
-            parseInt(item.style.transform.replace(/[^\d.-]/g, ""), 10) +
-            cardWidth;
-          item.style.transform = `translateX(${newTranslateXValue}px)`;
-        });
+        shuffleCards("leftToRight");
         counter--;
-        showCurrentCardNumber(counter);
       }
+      showCurrentCardNumber(counter);
     });
 
     leftAngleButton.addEventListener("click", () => {
       if (counter < cards.length) {
-        cards.forEach((item, index) => {
-          let newTranslateXValue =
-            parseInt(item.style.transform.replace(/[^\d.-]/g, ""), 10) -
-            cardWidth;
-          item.style.transform = `translateX(${newTranslateXValue}px)`;
-          if (index === counter) {
-            item.classList.add("activate");
-          }
-        });
+        shuffleCards("rightToLeft");
         counter++;
-        showCurrentCardNumber(counter);
       }
+      showCurrentCardNumber(counter);
     });
   }
 
@@ -52,6 +70,10 @@ window.onload = function() {
     el.innerHTML = `0${counter}`;
   }
 
+  setInitialStateForCards();
   showCurrentCardNumber(counter);
   toggleCardsFunctionality();
 };
+
+// item.style.transition =
+// "transform 1000ms cubic-bezier(0.82, 0, 0, 0.96)";
