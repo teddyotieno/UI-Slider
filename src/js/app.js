f8d3ee8b27b5f0669a1238c6ea169015d0fae85e
Teddy Otieno <teddy.asola@andela.com>
2018-08-17 14:35:43 +0300

    show card in view

--- src/js/app.js
+++ src/js/app.js
@@ -8,38 +8,47 @@ document.addEventListener("DOMContentLoaded", () => {
   // create initial state for the elements
   cards.forEach((item, index) => {
     item.style.transform = `translateX(${cardWidth * index}px)`;
-    // if (index !== 0) {
-    //   item.style.paddingLeft = "200px";
-    // }
   });
 
-  const rightAngleButton = document.getElementById("right-angle");
-  const leftAngleButton = document.getElementById("left-angle");
+  function toggleCardsFunctionality() {
+    const rightAngleButton = document.getElementById("right-angle");
+    const leftAngleButton = document.getElementById("left-angle");
 
-  rightAngleButton.addEventListener("click", () => {
-    if (counter > 1) {
-      cards.forEach(item => {
-        let newTranslateXValue =
-          parseInt(item.style.transform.replace(/[^\d.-]/g, ""), 10) +
-          cardWidth;
-        item.style.transform = `translateX(${newTranslateXValue}px)`;
-      });
-      counter--;
-    }
-  });
+    rightAngleButton.addEventListener("click", () => {
+      if (counter > 1) {
+        cards.forEach(item => {
+          let newTranslateXValue =
+            parseInt(item.style.transform.replace(/[^\d.-]/g, ""), 10) +
+            cardWidth;
+          item.style.transform = `translateX(${newTranslateXValue}px)`;
+        });
+        counter--;
+        showCurrentCardNumber(counter);
+      }
+    });
 
-  leftAngleButton.addEventListener("click", () => {
-    if (counter < cards.length) {
-      cards.forEach((item, index) => {
-        let newTranslateXValue =
-          parseInt(item.style.transform.replace(/[^\d.-]/g, ""), 10) -
-          cardWidth;
-        item.style.transform = `translateX(${newTranslateXValue}px)`;
-        // if (index === counter) {
-        //   item.style.paddingLeft = "0px";
-        // }
-      });
-      counter++;
-    }
-  });
+    leftAngleButton.addEventListener("click", () => {
+      if (counter < cards.length) {
+        cards.forEach((item, index) => {
+          let newTranslateXValue =
+            parseInt(item.style.transform.replace(/[^\d.-]/g, ""), 10) -
+            cardWidth;
+          item.style.transform = `translateX(${newTranslateXValue}px)`;
+          if (index === counter) {
+            item.classList.add("activate");
+          }
+        });
+        counter++;
+        showCurrentCardNumber(counter);
+      }
+    });
+  }
+
+  function showCurrentCardNumber(counter) {
+    const el = document.getElementById("card-number");
+    el.innerHTML = `0${counter}`;
+  }
+
+  showCurrentCardNumber(counter);
+  toggleCardsFunctionality();
 });
